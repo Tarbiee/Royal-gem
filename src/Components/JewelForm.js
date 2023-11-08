91dabcd38f6a43c37b444531293b62624f7eed11
Tarbiee <dorcusstaby@gmail.com>
2023-11-08 11:50:57 +0300

    Input text areas for jewelry form

--- src/Components/JewelForm.js
+++ src/Components/JewelForm.js
@@ -1,13 +1,47 @@
 import React from 'react';
+import { FloatingLabel } from 'react-bootstrap/FloatingLabel';
+import { Form } from 'react-bootstrap/Form';
 
 
 function JewelForm() {
   return (
     <div>
+        <div>
+            <div className='form'>
+            <FloatingLabel label="Name" className='mb-5'>
+             <Form.Control type="text"
+             name='name'
+             placeholder="Name" />
+            </FloatingLabel>
+            <FloatingLabel label="Image Url" className='mb-5'>
+             <Form.Control type="text"
+             name='image'
+             placeholder="ImageUrl" />
+            </FloatingLabel>
+            <FloatingLabel  label="Description" className='mb-5'>
+             <Form.Control type="text"
+             name='description' 
+             placeholder="Category" />
+            </FloatingLabel>
+            <FloatingLabel  label="Material" className='mb-5'>
+             <Form.Control type="text"
+             name='material' 
+             placeholder="Material" />
+            </FloatingLabel>
+            <FloatingLabel  label="Price" className='mb-5'>
+             <Form.Control type="text"
+             name='price' 
+             placeholder="Price" />
+            </FloatingLabel>
+            <Button variant="secondary">Add Jewelry</Button>
+               
+
+            </div>
+            </div>
         
       
     </div>
   )
 }
 
-export default JewelForm
+export default JewelForm;
