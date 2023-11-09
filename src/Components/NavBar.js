965dea279b6039633ad563bd63ff156516b3cc78
Tarbiee <dorcusstaby@gmail.com>
2023-11-09 12:37:35 +0300

    Import link from react-router-dom

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,24 +2,27 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 
 function NavBar() {
   return (
-    <div>
+    <router>
+      <div>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Jewelry</Nav.Link>
-            <Nav.Link href="#features">Add New</Nav.Link>
-            <Nav.Link href="#pricing">Cart</Nav.Link>
+            <Nav.Link as={Link} to={"/home"}>Jewelry</Nav.Link>
+            <Nav.Link as={Link} to={"/home"}>Add New</Nav.Link>
+            <Nav.Link as={Link} to={"/home"}>Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-
-      
+    
     </div>
+    </router>
+    
   )
 }
 
